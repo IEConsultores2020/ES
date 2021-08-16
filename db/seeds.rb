119156f0df74005038a1cb668fb5b00ad5ce23f9
Fernando Torres <fernando.torres@resilient-e.com>
2021-08-15 22:55:32 -0500

    es for svictorino

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+/to/seeds/20150928000000_initial_see
 
 r1 = Role.create({name: "Regular", description: "Can read items"})
 r2 = Role.create({name: "Seller", description: "Can read and create items. Can update and destroy own items"})
